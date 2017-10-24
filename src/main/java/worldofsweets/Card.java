6be9ed887da421d5d9d8ff30f771051444018545
James Finkel <32717956+jaf177@users.noreply.github.com>
2017-10-24 13:36:22 -0400

    final

---
+++ src/main/java/worldofsweets/Card.java
@@ -0,0 +1,55 @@
+//package worldofsweets;
+public class Card
+{
+  private String color;
+  private int value;
+  public Card(String c,int v)
+  {
+    setColor(c);
+    setValue(v);
+  }
+  public Card(int c,int v)
+  {
+    setColor(c);
+    setValue(v);
+  }
+
+  private void setColor(String c)
+  {
+    color = c;
+  }
+  private void setColor(int c)
+  {
+    String sc;
+    switch(c)
+    {
+      case 0: sc = "red"; break;
+      case 1: sc = "yellow"; break;
+      case 2: sc = "blue"; break;
+      case 3: sc = "green"; break;
+      case 4: sc = "orange"; break;
+      default: sc = "FAILURE"; break;
+    }
+    color = sc;
+  }
+  private void setValue(int v)
+  {
+    value = v;
+  }
+  public String getColor()
+  {
+    return color;
+  }
+  public int getValue()
+  {
+    return value;
+  }
+  public String toString()
+  {
+    if(value==1)
+    return "Single "+color;
+    else if(value==2)
+    return "Double "+color;
+    else return "FAILURE";
+  }
+}
---
+++ src/main/java/worldofsweets/Deck.java
@@ -0,0 +1,63 @@
+package worldofsweets;
+
+public class Deck
+{
+  private int size;
+  private Card[] deck;
+  private int nextCard;
+  public Deck()
+  {
+    size = 0;
+    nextCard=0;
+  }
+  public Deck(int s)
+  {
+    size = s;
+    deck = new Card[size];
+  }
+  public Card drawCard()
+  {
+    if(isEmpty())
+    shuffle();
+    nextCard++;
+    return deck[nextCard-1];
+  }
+  public void addCard(int color,int value)
+  {
+    addCard(new Card(color,value));
+  }
+  public void addCard(String color,int value)
+  {
+    addCard(new Card(color,value));
+  }
+  public void addCard(Card c)
+  {
+    Card[] d = new Card[size+1];
+    for(int x = 0;x<size;x++)
+    {
+      d[x]=deck[x];
+    }
+      d[size]=c;
+      size++;
+      deck = d;
+  }
+  public void shuffle()
+  {
+    for(int x=size-1;x>0;x--)
+    {
+      int r = (int)(Math.random()*(x+1));
+      Card temp = deck[x];
+      deck[x] = deck[r];
+      deck[r] = temp;
+    }
+    nextCard = 0;
+  }
+  public int getSize()
+  {
+    return size;
+  }
+  public boolean isEmpty()
+  {
+    return nextCard==size;
+  }
+}
