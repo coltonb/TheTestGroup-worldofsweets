f71ebfb2e7a7db9a397d07e6d9a50bbf3e2e712a
Randyll Bearer <rlb97@pitt.edu>
2017-11-01 18:03:31 -0400

    Refactored CardPanel + Added testCardPanel
    1. Cleaned up CardPanel code
    
    2. Commented CardPanel
    
    3. Added unit tests for CardPanel

--- src/main/java/worldofsweets/CardPanel.java
+++ src/main/java/worldofsweets/CardPanel.java
@@ -5,23 +5,29 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class CardPanel extends JPanel {
-    int cardsRemaining = 60;
-    int cardsDiscarded = 0;
-    Card lastCard;
-    Deck cardDeck = new Deck();
+    public int cardsRemaining = 60;	//how many cards are left in the deck, once this hits 0 need to shuffle
+    public int cardsDiscarded = 0;		//how many cards have been drawn/played, once this hits 60 needs to reset to 0
+	public int cardsPlayed = 0;		//how many cards have been played ALL GAME
+    public Card[] drawnCards = new Card[60];	//All the cards played in the game so far. Need to resize if we go over 60
+    public Deck cardDeck = new Deck();			//Deck of all the cards
 
+	public CardPanel(){
+		//do nothing
+	}
+	
     public CardPanel(WorldOfSweets game) {
-        this.setLayout(new FlowLayout());
+        //Create Panel
+		this.setLayout(new FlowLayout());
         
-        
-        //sub-panels
-        JPanel drew = new JPanel();
-        JPanel discard = new JPanel();
-        JPanel deck = new JPanel();
+        //Create sub-panels
+        JPanel drew = new JPanel();			//holds last card drawn
+        JPanel discard = new JPanel();		//holds number of cards played so far
+        JPanel deck = new JPanel();			//holds all cards remaining to be drawn
         this.add(drew);
         this.add(discard);
         this.add(deck);
         
+		//Customize sub-panels.
         drew.setBackground(Color.black);
         drew.setPreferredSize(new Dimension(150,300));
         discard.setBackground(Color.blue);
@@ -29,50 +35,66 @@ public class CardPanel extends JPanel {
         deck.setBackground(Color.red);
         deck.setPreferredSize(new Dimension(150,300));
         
-        //build deck 10 reg, 2 double, 5 colors
-        cardDeck = buildDeck();
+        //Builds the deck, 10 single per color, 2 double per color, 5 colors.
+        cardDeck = new Deck();
+		cardDeck.shuffle();
         
-        //Buttons
+        //Create Buttons
         JButton discardPile = new JButton("Cards Discarded: " + cardsDiscarded);
-        
         JButton cardDrawn = new JButton("Card Drawn: ");
         JButton card = new JButton("");
-        
+		
         JButton drawCard = new JButton("Draw Card: " + cardsRemaining);
         drawCard.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e){	//handle all logic which follows the drawing of a card.
                 if(cardsRemaining == 0){
-                    cardDeck = buildDeck();
+                    cardDeck = new Deck();				//reshuffle the deck
+					cardDeck.shuffle();
                     cardsRemaining = 60;
                     cardsDiscarded = 0;
+					drawnCards = resizeDrawnCards();	//allow drawnCards to hold more cards
                 }
 
+				//Draw Card
+				Card newCard = drawCard(cardDeck);
+				
+				//update drawnCards[]
+				drawnCards[cardsPlayed] = newCard;
+				
+				//Update Information
+				cardsPlayed = cardsPlayed + 1;
                 cardsRemaining = cardsRemaining - 1;
                 cardsDiscarded = cardsDiscarded + 1;
+				
+				//Update sub-panels
                 drawCard.setText("Draw Card: " + cardsRemaining);
                 discardPile.setText("Cards Discarded: " + cardsDiscarded);
-                Card newCard = drawCard(cardDeck);
-                lastCard = newCard;
                 card.setText(newCard.getColor() + " " + newCard.getValue());
                 
-                //send update to gameFrame
-                game.makeMove(drawCard());
-                
+                //Send update to WorldOfSweets
+                game.makeMove(newCard);
             }
             
         });	
-        
-        //add buttons to panel
+        //add buttons to panel + draw
         deck.add(drawCard);
         discard.add(discardPile);
         drew.add(cardDrawn);
         drew.add(card);
+		this.setVisible(true);
         
     }
     
-    public Deck buildDeck(){
+	//Create the deck of cards to be used in the game
+	//May not be used if buildingDeck is moved to Deck class
+	//As it stands:
+	//Red: 10 single + 2 Double
+	//Blue: 10 single + 2 double
+	//Yellow: 10 single + 2 double
+	//Green: 10 single + 2 double
+	//Orange: 10 single + 2 double
+    public static Deck buildDeck(){
         Deck deck = new Deck();
-        
         int i = 0;
         Card toAdd;
         while(i < 12){	//blue
@@ -128,19 +150,52 @@ public class CardPanel extends JPanel {
             deck.addCard(toAdd);
             i ++;
         }
-        
         deck.shuffle();
         return deck;
     }
     
-    //Externall call this to get the last card drawn
-    public Card drawCard(){
-        return lastCard;
+    //External call this to get the last card played
+    public Card getLastDrawn(){
+        return drawnCards[cardsPlayed-1];
     }
-    
-    public Card drawCard(Deck deckOfCards){
+	
+	//External call this to get the last "x" amount of cards drawn
+    public Card[] getLastDrawn(int x){
+		Card[] toReturn = new Card[x];
+		int i = 0;
+		while (i < x){
+			toReturn[i] = drawnCards[i];
+			i = i + 1;
+		}
+		return toReturn;
+	}
+	
+	//Draw a single card from the deck
+    public static Card drawCard(Deck deckOfCards){
         Card result = deckOfCards.drawCard();
         return result;
     }
+	
+	//Draw "x" amount of cards from the deck
+	public static Card[] drawCard(Deck deckOfCards, int x){
+		Card[] requestedCards = new Card[x];
+		int i = 0;
+		while(i < x){
+			requestedCards[i] = deckOfCards.drawCard();
+			i = i + 1;
+		}
+		return requestedCards;
+	}
+	
+	//double the size of drawnCards[] so that it can hold more history
+	public Card[] resizeDrawnCards(){
+		Card[] newDrawnCards = new Card[(drawnCards.length * 2)];
+		int i = 0;
+		while(i < drawnCards.length){
+			newDrawnCards[i] = drawnCards[i];
+			i = i + 1;
+		}
+		return newDrawnCards;
+	}
     
 }
