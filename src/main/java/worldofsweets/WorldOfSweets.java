e5cf8d3dbdd11374dc7adab96018ad07ef1e464f
Colton Blake <coltonjmsblake@gmail.com>
2017-11-06 15:43:42 -0500

    displaying player turn now own method

--- src/main/java/worldofsweets/WorldOfSweets.java
+++ src/main/java/worldofsweets/WorldOfSweets.java
@@ -12,7 +12,7 @@ public class WorldOfSweets {
     public static final int TILE_HEIGHT = 7;
 
     private int numPlayers;
-    private int currentPlayer = 0;
+    private int currentPlayer;
     Player[] players;
 
     private GameFrame frame;
@@ -61,6 +61,7 @@ public class WorldOfSweets {
         numPlayers = promptNumPlayers();
 
         players = new Player[numPlayers];
+        currentPlayer = 0;
 
         String[] names = promptPlayerNames(numPlayers);
 
@@ -68,6 +69,7 @@ public class WorldOfSweets {
             players[i] = new Player();
             players[i].setName(names[i]);
         }
+
 
         board = new Board();
         board.addPlayers(players);
@@ -82,12 +84,15 @@ public class WorldOfSweets {
         frame.setVisible(true);
 
         //prompt player
+        promptPlayerTurn(players[currentPlayer]);
+    }
+
+    private void promptPlayerTurn(Player player) {
         JOptionPane.showMessageDialog(
             null,
-            players[currentPlayer].getName() + ", it's your turn!",
+            player.getName() + ", it's your turn!",
             "Player Confirmation",
             JOptionPane.INFORMATION_MESSAGE);
-
     }
 
     /*
@@ -161,12 +166,7 @@ public class WorldOfSweets {
         currentPlayer = (currentPlayer + 1) % players.length;
 
         //prompt player
-        JOptionPane.showMessageDialog(
-            null,
-            players[currentPlayer].getName() + ", it's your turn!",
-            "Player Confirmation",
-            JOptionPane.INFORMATION_MESSAGE);
-
+        promptPlayerTurn(players[currentPlayer]);
     }
 
     public static void main(String[] args) {
